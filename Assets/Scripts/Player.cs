6383177faa426e05780f31ac05b994e185bc30c6
Chia Sheng Jie <chia0360@e.ntu.edu.sg>
2017-08-17 10:56:59 +0800

    Done Idle Animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y); // Consist of x = -1, y = 0 values.
+    }
+}
